edf935b14fd6acefe15ba944e46c3570d5838a51
Mokeev Andrey <andrey@mokeev1995.ru>
2015-11-01 23:43:37 +0300

    news working

--- app/Admin/Models/Categories.php
+++ app/Admin/Models/Categories.php
@@ -1,7 +1,19 @@
 <?php
-	/**
-	 * Created by PhpStorm.
-	 * User: mokeev1995
-	 * Date: 01.11.2015
-	 * Time: 23:19
-	 */
+
+	Admin::model(App\Models\Category::class)->title('Категории')->display(function ()
+	{
+		$display = AdminDisplay::table();
+
+		$display->columns([
+			Column::string('id')->label('Id'),
+			Column::string('name')->label('Название категории'),
+		]);
+		return $display;
+	})->createAndEdit(function ()
+	{
+		$form = AdminForm::form();
+		$form->items([
+			FormItem::text('name','Название категории')->required()->unique(),
+		]);
+		return $form;
+	});
