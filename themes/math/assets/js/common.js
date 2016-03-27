d92df25035505019171229df05edbc1efc521b91
Mokeev Andrey <andrey@mokeev1995.ru>
2016-03-27 13:45:27 +0300

    add new features

---
+++ themes/math/assets/js/common.js
@@ -0,0 +1,22 @@
+$(function() {
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	}
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	}
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
