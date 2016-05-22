04b28a0dd9c5d0c87723e7fb1c2474a1a0eca4ae
Mokeev Andrey <andrey@mokeev1995.ru>
2016-05-23 02:01:23 +0300

    removed demo plugin && extended blogExtender plugin

--- plugins/mokeev1995/blogextender/Plugin.php
+++ plugins/mokeev1995/blogextender/Plugin.php
@@ -1,14 +1,54 @@
 <?php namespace mokeev1995\BlogExtender;
 
+use RainLab\Blog\Controllers\Posts;
+use RainLab\Blog\Models\Post;
 use System\Classes\PluginBase;
+use Backend\Widgets\Form;
+use Backend\Widgets\Lists;
 
 class Plugin extends PluginBase
 {
-    public function registerComponents()
-    {
-    }
+	public $require = ['RainLab.Blog'];
 
-    public function registerSettings()
-    {
-    }
+	public function registerComponents()
+	{
+	}
+
+	public function registerSettings()
+	{
+	}
+
+	public function boot()
+	{
+		Posts::extendListColumns(function (Lists $list, $model){
+			if (!$model instanceof Post)
+			{
+				return;
+			}
+
+			$list->addColumns([
+				'front_page_visible' => [
+					'label' => "Отображается на главной",
+					'type'  => 'switch',
+				]
+			]);
+		});
+
+		Posts::extendFormFields(function (Form $form, $model, $context)
+		{
+
+			if (!$model instanceof Post)
+			{
+				return;
+			}
+
+			$form->addSecondaryTabFields([
+				'front_page_visible' => [
+					'label' => "Отображать на главной странице",
+					'tab'   => "rainlab.blog::lang.post.tab_manage",
+					'type'  => 'checkbox',
+				],
+			]);
+		});
+	}
 }
